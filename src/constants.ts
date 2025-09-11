781c6f84eaab4c3f563433faee67dcc7281752c1
Roger Rutishauser <roger.rutishauser@uzh.ch>
2025-09-11 10:06:43 +0200

    update matomo tracker

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,5 @@
 export const pageinfo = {
   title: "Historische Vorlesungsverzeichnisse der Universität Zürich",
-  byline: "1833–1917",
   description: "Das Portal &laquo;Historische Vorlesungsverzeichnisse der Universität Zürich&raquo; erschliesst als Hilfsmittel und Nachschlagewerk für biographische, forschungs-, disziplinen- oder institutionsgeschichtliche Fragestellungen die an der Universität Zürich angekündigten Lehrveranstaltungen auf der Grundlage der gedruckten Vorlesungsverzeichnisse (1833–2014). Die Datenbank ist noch nicht vollständig, wird aber in unregelmässigen Abständen erweitert.",
-  matomo_tracking_id: 69
+  matomo_tracking_id: "69"
 };
